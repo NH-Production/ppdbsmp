a8b9616d47f12e5489557d9231b369e7959c7ff8
NH PRODUCTION <178999430+NH-Production@users.noreply.github.com>
2026-06-29 09:01:16 +0700

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // Service to interact with Google Apps Script Backend
 
 // To use the real backend, replace this URL with your deployed Google Apps Script Web App URL
-const GAS_WEB_APP_URL = ""; 
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyYsdJHC4O9bmYg_wqNBSHeo5Ykb0HZ6msFaAcEarS9-LU4mqGBYhQ9bo1VKi4grXU/exec"; 
 
 export interface FormField {
   id: string;
